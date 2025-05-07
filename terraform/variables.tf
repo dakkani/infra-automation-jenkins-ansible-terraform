5551d161d5c8a105e6d2bdd07f6264c17f61382a
dakkani <135724674+dakkani@users.noreply.github.com>
2025-05-08 00:09:22 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,6 +5,11 @@ variable "r9_username" {
 variable "d12_username" {
   default = "admin"
 }
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default    = "us-east-1"
+}
 
 variable "public_key" {
   description = "SSH public key for VM access"
